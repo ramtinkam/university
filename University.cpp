f9e9bc085ebcb258ce1664a6d9e8f7a9d8348066
ramtinkam <ramtinkamanian82@gmail.com>
2022-06-13 23:38:48 +0430

    overloaded << and >> operators for University class

--- University.cpp
+++ University.cpp
@@ -43,3 +43,120 @@ University::~University(){
     delete []sArray;
     delete []pArray;
 }
+
+int University::getBudget()const{return budget;}
+int University::getNumOfProfessors()const{return numOfProfessors;}
+int University::getNumOfStudents()const{return numOfStudents;}
+Professor* University::getpArray()const{return pArray;}
+Student* University::getsArray()const{return sArray;}
+
+void University::setBudget(int b){budget=b;}
+void University::setNumOfPorfessors(int np){numOfProfessors=np;}
+void University::setNumOfStudents(int ns){numOfStudents=ns;}
+void University::setpArray(Professor* p){pArray=p;}
+void University::setsArray(Student* s){sArray=s;}
+
+void University::sort(Student* a){
+    int year[numOfStudents];
+    for(int i=0;i<numOfStudents;i++){
+        string id=a[i].getid();
+        id.erase(id.begin()+2,id.end());
+        year[i]=stoi(id);
+        if(year[i]==0){
+            year[i]=100;
+        }
+    }
+    int temp;
+    Student sTemp;
+    for(int i=0;i<numOfStudents-1;i++){
+        for(int j=0;j<numOfStudents-i-1;j++){
+            if(year[j]>year[j+1]){
+                temp = year[j];
+                year[j]=year[j+1];
+                year[j+1]=temp;
+                sTemp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=sTemp;
+            }
+            else if(year[j]==year[j+1] && a[j].getlastname()>a[j+1].getlastname()){
+                temp = year[j];
+                year[j]=year[j+1];
+                year[j+1]=temp;
+                sTemp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=sTemp;
+            }
+        }
+
+    }
+}
+void University::sort(Professor* a){
+    int year[numOfProfessors];
+    for(int i=0;i<numOfProfessors;i++){
+        string id=a[i].getid();
+        id.erase(id.begin()+2,id.end());
+        year[i]=stoi(id);
+        if(year[i]==0){
+            year[i]=100;
+        }
+    }
+    int temp;
+    Professor pTemp;
+    for(int i=0;i<numOfProfessors-1;i++){
+        for(int j=0;j<numOfProfessors-i-1;j++){
+            if(year[j]>year[j+1]){
+                temp = year[j];
+                year[j]=year[j+1];
+                year[j+1]=temp;
+                pTemp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=pTemp;
+            }
+            else if(year[j]==year[j+1] && a[j].getlastname()>a[j+1].getlastname()){
+                temp = year[j];
+                year[j]=year[j+1];
+                year[j+1]=temp;
+                pTemp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=pTemp;
+            }
+        }
+
+    }
+}
+
+ostream& operator <<(ostream& out,University& a){
+    out<<"budget: "<<a.getBudget()<<" numOfProfessors:"<<a.getNumOfProfessors()<<" numOfStudents:"<<a.getNumOfStudents()<<"\n";
+    a.sort(a.getpArray());
+    a.sort(a.getsArray());
+    Professor* x=a.getpArray();
+    Student* y=a.getsArray(); 
+    for(int i=0;i<a.getNumOfProfessors();i++){
+        out<<x[i];
+    }
+    for(int i=0;i<a.getNumOfStudents();i++){
+        out<<y[i];
+    }
+    out<<"\n";
+    return out;
+
+}
+istream& operator>>(istream& in,University& a){
+    int b,np,ns;
+    cout<<"enter budget, numOfProfessors and numOfStudents\n";
+    in>>b>>np>>ns;
+    a.setBudget(b);
+    a.setNumOfPorfessors(np);
+    a.setNumOfStudents(ns);
+    Professor* p=new Professor[np];
+    Student* s=new Student[ns];
+    for(int i=0;i<np;i++){
+        in>>p[i];
+    }
+    for(int i=0;i<ns;i++){
+        in>>s[i];
+    }
+    a.setpArray(p);
+    a.setsArray(s);
+    return in;
+}
--- University.h
+++ University.h
@@ -21,6 +21,21 @@ using namespace std;
         University(int,int,int,Professor*,Student*);
         University(const University&);
         ~University();
+        int getBudget()const;
+        int getNumOfProfessors()const;
+        int getNumOfStudents()const;
+        Professor* getpArray()const;
+        Student* getsArray()const;
+        void setBudget(int);
+        void setNumOfPorfessors(int);
+        void setNumOfStudents(int);
+        void setpArray(Professor*);
+        void setsArray(Student*);
+        void sort(Student*);
+        void sort(Professor*);
+        friend ostream& operator <<(ostream& ,University&);
+        friend istream& operator >>(istream&, University&);
+
     };
 
 
